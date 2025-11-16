6ff0b4064f27f55333b46fbb0ad7b1ae49543224
Ferni <49432975@est.ort.edu.ar>
2025-11-16 19:14:55 -0300

    Update teleport-points.js

--- ATI_VR_PROYECTO/js/teleport-points.js
+++ ATI_VR_PROYECTO/js/teleport-points.js
@@ -1,5 +1,5 @@
 export const TELEPORT_POINTS = [
-  { id: 'tp1', position: { x: 0, y: 1.6, z: -3 } },
-  { id: 'tp2', position: { x: 5, y: 1.6, z: -2 } },
-  { id: 'tp3', position: { x: -4, y: 1.6, z: -1 } }
+  { id: 'tp1', position: { x: 0, y: 2, z: -3 } },
+  { id: 'tp2', position: { x: 5, y: 2, z: -2 } },
+  { id: 'tp3', position: { x: -4, y: 2, z: -1 } }
 ];
