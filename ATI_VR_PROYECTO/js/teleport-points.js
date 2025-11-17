fd262eeac4248d35d42b6b0d7cc1ab68d49cf3e8
Ferni <49432975@est.ort.edu.ar>
2025-11-17 02:20:27 -0300

    Color and INROOM TP test

--- ATI_VR_PROYECTO/js/teleport-points.js
+++ ATI_VR_PROYECTO/js/teleport-points.js
@@ -3,16 +3,16 @@ export const TELEPORT_POINTS = [
     room: 'uno',
     position: { x: -3, y: 1.6, z: 2.6 }, //Z VA INVERSO y es Y!!
     teleportTarget: { x: 0, y: 1.6, z: 0 },
-    color: '#e418ffff',
+    color: '#FF00FF',
     label: '',
     movesToOtherRoom: true,
     targetRoom: 'dos'
   },
   {
     room: 'dos',
-    position: { x: 0.5, y: 1, z: -11 },
+    position: { x: 0.5, y: 1, z: 11 },
     teleportTarget: { x: 0, y: 1.6, z: 0  },
-    color: '#ff18ffff',
+    color: '#FF00FF',
     label: '',
     movesToOtherRoom: true,
     targetRoom: 'tres'
@@ -21,52 +21,52 @@ export const TELEPORT_POINTS = [
     room: 'dos',
     position: { x: -2, y: 1.6, z: 4.3 },
     teleportTarget: { x: -2, y: 1.6, z: 4.3 },
-    color: '#1869ffff',
+    color: '#00AFFF',
     label: '',
-    movesToOtherRoom: false,
-
+    movesToOtherRoom: true,
+    targetRoom: 'dos'
   },
   {
     room: 'dos',
     position: { x: 3.5, y: 1.6, z: 5.1 },
     teleportTarget: { x: 3.5, y: 1.6, z: 5.1  },
-    color: '#1869ffff',
+    color: '#00AFFF',
     label: '',
-    movesToOtherRoom: false,
-
+    movesToOtherRoom: true,
+    targetRoom: 'dos'
   },
   {
     room: 'dos',
     position: { x: 3.5, y: 1.6, z: -1 },
     teleportTarget: { x: 3.5, y: 1.6, z: -1  },
-    color: '#1869ffff',
+    color: '#00AFFF',
     label: '',
-    movesToOtherRoom: false,
-
+    movesToOtherRoom: true,
+    targetRoom: 'dos'
   },
   {
     room: 'dos',
     position: { x: 3.5, y: 1.6, z: -11 },
     teleportTarget: { x: 3.5, y: 1.6, z: -11  },
-    color: '#1869ffff',
+    color: '#00AFFF',
     label: '',
-    movesToOtherRoom: false,
-
+    movesToOtherRoom: true,
+    targetRoom: 'dos'
   },
     {
     room: 'dos',
     position: { x: -1.7, y: 1.6, z: -11 },
     teleportTarget: { x: -1.7, y: 1.6, z: -11  },
-    color: '#1869ffff',
+    color: '#00AFFF',
     label: '',
-    movesToOtherRoom: false,
-
+    movesToOtherRoom: true,
+    targetRoom: 'dos'
   },
   {
     room: 'tres',
     position: { x: -4, y: 1.6, z: 0 },
     teleportTarget: { x: 0, y: 1.6, z: 0  },
-    color: '#ee22eeff',
+    color: '#FF00FF',
     label: '',
     movesToOtherRoom: true,
     targetRoom: 'cuatro'
@@ -75,7 +75,7 @@ export const TELEPORT_POINTS = [
     room: 'cuatro',
     position: { x: -1, y: 1, z: 3 },
     teleportTarget: { x: 0, y: 1.6, z: 0  },
-    color: '#ff18d8ff',
+    color: '#FF00FF',
     label: '',
     movesToOtherRoom: true,
     targetRoom: 'cinco'
@@ -84,23 +84,25 @@ export const TELEPORT_POINTS = [
     room: 'cuatro',
     position: { x: 7, y: 1.6, z: 1 },
     teleportTarget: { x: 7, y: 1.6, z: 3  },
-    color: '#181cffff',
+    color: '#00AFFF',
     label: '',
-    movesToOtherRoom: false,
+    movesToOtherRoom: true,
+    targetRoom: 'cuatro'
   },
   {
     room: 'cuatro',
     position: { x: 7, y: 1.6, z: -3 },
     teleportTarget: { x: 7, y: 1.6, z: -3  },
-    color: '#181cffff',
+    color: '#00AFFF',
     label: '',
-    movesToOtherRoom: false,
+    movesToOtherRoom: true,
+    targetRoom: 'cuatro'
   },
   {
     room: 'cinco',
     position: { x: -4, y: 1.6, z: -1.5 },
     teleportTarget: { x: 0, y: 1.6, z: 0  },
-    color: '#ed16f5ff',
+    color: '#FF00FF',
     label: '',
     movesToOtherRoom: true,
     targetRoom: 'seis'
@@ -109,7 +111,7 @@ export const TELEPORT_POINTS = [
     room: 'seis',
     position: { x: 2.5, y: 1.5, z: 1.2 },
     teleportTarget: { x: 0, y: 1.6, z: 0  },
-    color: '#be18ffff',
+    color: '#FF00FF',
     label: '',
     movesToOtherRoom: true,
     targetRoom: 'uno'
